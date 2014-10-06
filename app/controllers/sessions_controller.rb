e1da55b8c576b2051fe3f538f4d1730e8f54569c
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-10-06 09:49:02 -0600

    Think it's working?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,9 +16,11 @@ class SessionsController < ApplicationController
       if @user.logins == 0
         p "logins are 0"
         @user.login_attempt_counter
+        redirect_to :back
       else
-        p "logins are more than 0"
+        p "logins are more than 4"
         @user.wait_2_minutes
+        redirect_to :back
       end
       @user.check_user_logins
       render :new
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,26 +4,30 @@ class User < ActiveRecord::Base
 
 
   def login_attempt_counter
-    #everytime there is a bad login attempt update the counter in the sessions database
-  end
-
-  def check_user_logins
-   #when there is a bad attempt, check the sessions db to see if they have reached the maximum
-  end
-
-  def erase_logins
-    self.update(logins: 0)
-  end
-
-  def wait_1_minute
-    if
-     # if the current time is less than 1 minute since the last bad attempt, update the counter again
-    else
-      erase_logins
-      login_attempt_counter
+      self.increment_counter(:logins, 0)
     end
+
+    def check_user_logins
+      if self.logins >= 4
+        self.wait_1_minute
+      else
+        self.login_attempt_counter
+      end
+    end
+
+    def erase_logins
+      self.update(logins: 0)
+    end
+
+    def wait_1_minute
+      if (DateTime.now.to_i - self.updated_at.to_i) < 60   
+       self.update(login_attempt_counter)
+
+      else
+        erase_logins
+        login_attempt_counter
+      end
+    end
+
+
   end
-
-
-
-end
